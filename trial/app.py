c37be180b14610db8d84c7774d6f92e00cc72489
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-01-07 11:14:08 +0400

    Fixed dependency issue

--- trial/app.py
+++ trial/app.py
@@ -9,7 +9,7 @@ from jaeger_client import Config
 from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from prometheus_flask_exporter import PrometheusMetrics
+# from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 
@@ -19,7 +19,7 @@ RequestsInstrumentor().instrument()
 
 metrics = GunicornInternalPrometheusMetrics(app)
 # static information as metric
-metrics.info("app_info", "Application info", version="1.0.3")
+# metrics.info("app_info", "Application info", version="1.0.3")
 
 logging.getLogger("").handlers = []
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
@@ -36,7 +36,7 @@ def init_tracer(service):
         },
         service_name=service,
         validate=True,
-        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
+        # metrics_factory=PrometheusMetricsFactory(service_name_label=service),
     )
 
     # this call also sets opentracing.tracer
